4d6988b5d6b18f4065ca7f8ca763fa5f779a8f45
Pablo E <pabloesquilache@gmail.com>
2022-05-12 13:35:39 -0300

    quintoCOmmit

--- javascript/estilo.js
+++ javascript/estilo.js
@@ -8,5 +8,5 @@ btnEnvio.addEventListener('click', function(e) {
     const ausunto = document.getElementById("asunto").value;
     const mensaje = document.getElementById("mensaje").value;
     const file = document.getElementById("file").value;
-    window.location.href='mailto:pabloesquilache@gmail.com?subjet=envioDesdeFormulario&body=Nombre%20y%20Apellido%3A${nombre}%0Aemail%3A{email}%0AAsunto%3A{asunto}%0AMensaje%3A{mensaje}%0AFile%3A{file}';
+    window.location.href='mailto:pabloesquilache@gmail.com?body=Nombre%20y%20Apellido%3A${nombre}%0Aemail%3A ${email}%0AAsunto%3A ${asunto}%0AMensaje%3A ${mensaje}%0AFile%3A ${file}';
 });
