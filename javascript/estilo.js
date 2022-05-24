b8d47ebdb7436df555e1f0b5c9b4b3fe5fca6a82
Pablo E <pabloesquilache@gmail.com>
2022-05-24 13:21:44 -0300

    Final1

--- javascript/estilo.js
+++ javascript/estilo.js
@@ -20,7 +20,6 @@ btnEnvio.addEventListener('click', function(e) {
     const nombre = document.getElementById("nombre").value;
     const email = document.getElementById("email").value;
     const asunto = document.getElementById("asunto").value;
-    const mensaje = document.getElementById("mensaje").value; 
-    const file = document.getElementById("file").value;
-    window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=Nombre%20y%20Apellido%3A${nombre}%0Aemail%3A${email}%0AMensaje%3A${mensaje}%0AFile%3A${file}`;
+    const mensaje = document.getElementById("mensaje").value;
+    window.location.href=`mailto:pabloesquilache@gmail.com?subject=${asunto}&body=Nombre%20y%20Apellido%3A${nombre}%0Aemail%3A${email}%0AMensaje%3A${mensaje}`;
 });
